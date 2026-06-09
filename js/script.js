988c4a5ccd10aecc503a4a39c2a5040cb3517954
maxtumba <max.tumba@elev.ga.ntig.se>
2026-06-09 11:48:49 +0200

    stylat bättre

--- js/script.js
+++ js/script.js
@@ -6,20 +6,28 @@ document.addEventListener("keydown", (e) => {
     const key = e.key.toLowerCase();
 
     const el = document.querySelector(`[data-key="${key}"]`)
+
     if (el) {
         el.classList.add("active");
     }
+    
     audio.currentTime = 0;
 
     audio.play();
+
+    console.log("key:", e.key);
+    console.log("code:", e.code);
+
 });
 
 document.addEventListener("keyup", (e) => {
     const key= e.key.toLowerCase();
-
+    
     const el = document.querySelector(`[data-key="${key}"]`);
+
     if (el)
     {
         el.classList.remove("active");
     }
+   
 });
